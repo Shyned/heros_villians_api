fc01a478acccb00b8ebcedcae01bd621a47a1b44
Dyshine Hill <eyedolarts@gmail.com>
2022-04-04 13:57:07 -0500

    Created models for supers and super_types + created a local url for supers

--- super_types/models.py
+++ super_types/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Super_Type(models.Model):
+    type = models.Charfield(max_length = 100)
+    
--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from super_types import Super_Type
 
 # Create your models here.
+
+class Super(models.Model):
+    name = models.Charfield(max_length = 100)
+    alter_ego = models.Charfield(max_length = 100)
+    primary_ability = models.Charfield(max_length= 100)
+    secondary_ability = models.Charfield(max_length=100)
+    good_evil = models.ForeignKey(Super_Type, on_delete = models.CASCADE)

d6b378249e514999f6b729dca4892b884d0e9925
Dyshine Hill <eyedolarts@gmail.com>
2022-04-06 14:18:26 -0500

    created a function to view and accept paramters

--- super_types/models.py
+++ super_types/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Super_Type(models.Model):
     
-    hero_or_villian = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
--- supers/views.py
+++ supers/views.py
@@ -9,15 +9,31 @@ from .models import Super
 
 @api_view(['GET','POST'])
 def get_super_list(request):
-
+    super_type_param= request.query_params.get('super_types')
+    type_param = request.query_params.get('type')
+    supers = Super.objects.all()
+    super_heros=supers.filter(super_type__type='hero')
+    super_villains=supers.filter(super_type__type='villain')
     # 1.Accepts a value from the request's URL (The id of the super to retrieve).
     # 2.Returns a 200 status code.
     # 3.Responds with the super in the database that has the id that was sent through the URL
     
     if request.method == 'GET':
-        supers = Super.objects.all()
-        serializer = SuperSerializer(supers, many =True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
+        if type_param == 'hero':
+            
+                
+            serializer = SuperSerializer(super_heros,many=True)
+            return Response(serializer.data)
+            
+        elif type_param=='villain':
+            serializer = SuperSerializer(super_villains,many=True)
+            return Response(serializer.data)
+
+        else:
+            serializer = SuperSerializer(supers, many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        
     
     # Adds the new super to the database.
     # Returns a 201 status code.
@@ -53,6 +69,4 @@ def change_super(request,pk):
     elif request.method == 'DELETE':
         supers.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 
